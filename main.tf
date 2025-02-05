8c69377365a8e74a4b67f4478ba923d2af013ad8
jbartus <561971+jbartus@users.noreply.github.com>
2025-02-05 18:03:54 -0500

    tighten formatting a bit

--- main.tf
+++ main.tf
@@ -3,20 +3,22 @@ provider "aws" {
 }
 
 module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  name               = "eks-vpc"
-  azs                = ["us-east-1a", "us-east-1c", "us-east-1d"]
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  source          = "terraform-aws-modules/vpc/aws"
+  name            = "eks-vpc"
+  azs             = ["us-east-1a", "us-east-1c", "us-east-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   enable_nat_gateway = true
   single_nat_gateway = true
 }
 
 module "eks" {
-  source                                   = "terraform-aws-modules/eks/aws"
-  cluster_name                             = "my-eks-cluster"
-  vpc_id                                   = module.vpc.vpc_id
-  subnet_ids                               = module.vpc.private_subnets
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = "my-eks-cluster"
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
